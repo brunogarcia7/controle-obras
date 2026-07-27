a14aa4c341d7588e2a003ed8e9c3505407bcb272
brunogarcia7 <119528794+brunogarcia7@users.noreply.github.com>
2026-07-27 13:33:00 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     SUPABASE_URL: 'https://aidlesbrbwfxpziivkgw.supabase.co',
-    SUPABASE_KEY: 'aidlesbrbwfxpziivkgw',
+    SUPABASE_KEY: 'sb_publishable_1wUqabj2T6y9L5tC-tAnwA_bzj-fyx0',
     COLUNAS_PADRAO: { obra: true, equip: true, periodo: true, contrato: true, valor: true, anexo: true, acoes: true }
 };
 
